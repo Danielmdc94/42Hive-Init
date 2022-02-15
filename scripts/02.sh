9375c01f8f27dd8a9be0f2579d129c233467b07f
Daniel Palacio <dpalacio@student.hive.fi>
2022-02-15 17:57:56 +0200

    scrips/02 modified

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 yes=yes
-no=no
 
 echo "Type the user you want to delete:"
 read user
-echo "Are you sure you want to remove $user and all its files?[yes/no]"
+echo "Are you sure you want to remove the user '$user' and all its files?[yes/no]"
 read answer
 if [ "$answer" == "$yes" ]
 then
